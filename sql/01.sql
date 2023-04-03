6c21097e459cd207c22f99fa2e08ca3b769cd91d
Nishka <nishka.ayyar@gmail.com>
2023-04-03 14:57:19 -0700

    next try

--- sql/01.sql
+++ sql/01.sql
@@ -1,5 +1,5 @@
 /*
- * Display the first and last name of each actor in a single column in upper case letters.
+ * hi Display the first and last name of each actor in a single column in upper case letters.
  * Name the column Actor Name, and sort the results alphabetically.
  */
 
